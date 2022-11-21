b824b4fb25a17cc2c9b2fd06e036b8f09a11795a
Nic <85948162+nicguzz@users.noreply.github.com>
2022-11-21 13:07:54 +0100

    mobile final

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,4 +1,54 @@
 <template>
+  <Disclosure as="nav" class="bg-teal-600" v-slot="{ open }">
+    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <div class="relative flex h-16 items-center justify-between">
+        <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+          <!-- Mobile menu button-->
+          <DisclosureButton
+            class="inline-flex items-center justify-center rounded-md p-2 text-white hover:bg-teal-800 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+          >
+            <span class="sr-only">Open main menu</span>
+            <Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
+            <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
+          </DisclosureButton>
+        </div>
+        <div
+          class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-center"
+        >
+          <div class="hidden sm:ml-6 sm:block">
+            <div class="flex space-x-4">
+              <a
+                v-for="item in navigation"
+                :key="item.name"
+                :href="item.href"
+                class="text-gray-300 hover:bg-teal-700 px-3 py-2 rounded-md text-lg font-medium"
+                :aria-current="item.current ? 'page' : undefined"
+                >{{ item.name }}</a
+              >
+            </div>
+          </div>
+        </div>
+        <div
+          class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"
+        ></div>
+      </div>
+    </div>
+
+    <DisclosurePanel class="sm:hidden">
+      <div class="space-y-1 px-2 pt-2 pb-3">
+        <DisclosureButton
+          v-for="item in navigation"
+          :key="item.name"
+          as="a"
+          :href="item.href"
+          class="text-gray-300 hover:bg-teal-700 block px-3 py-2 rounded-md text-xl font-medium text-center"
+          :aria-current="item.current ? 'page' : undefined"
+          >{{ item.name }}</DisclosureButton
+        >
+      </div>
+    </DisclosurePanel>
+  </Disclosure>
+  <!-- 
   <div class="nav">
     <div class="nav-sections">
       <ul class="nav-list">
@@ -34,9 +84,27 @@
         </li>
       </ul>
     </div>
-  </div>
+  </div> -->
 </template>
 
-<script setup></script>
+<script setup>
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+} from "@headlessui/vue";
+import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/vue/24/outline";
+
+const navigation = [
+  { name: "Skills", href: "#skills", current: true },
+  { name: "Projects", href: "#projects", current: false },
+  { name: "Education", href: "#education", current: false },
+  { name: "Contact", href: "#contact", current: false },
+];
+</script>
 
 <style></style>
